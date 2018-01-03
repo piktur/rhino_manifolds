a2d87d2775d38d3ed7be03369b69ec8a1b7496f1
Daniel Small <piktur.io@gmail.com>
2018-01-04 03:08:39 +1100

    Document proposed export loop

--- lib/calabi/export.py
+++ lib/calabi/export.py
@@ -1,2 +1,9 @@
 # @todo Export Points to File. Useful in cases where we might wish to generate models from the points array with tools other than Rhino.
 # @see ~/Library/Application Support/McNeel/Rhinoceros/Scripts/samples/ExportPoints.py
+
+# @example Export
+# for dimension in range(10):
+#     step = 15 # ensure multiple of 15
+#     for i in range((360 / step) - 1): # No need to export at both 0 and 360deg
+#         rotation = i * step
+#         print {'dim': dimension, 'x': rotation}
